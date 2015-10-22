433a25c525892b180d0473aa8351d562dfff77a4
Marcon <bielmarcon@hotmail.com>
2015-10-22 21:49:59 -0200

    fragmento a 1.0

--- app/src/main/java/com/nutritime2/FragmentoA.java
+++ app/src/main/java/com/nutritime2/FragmentoA.java
@@ -1,18 +1,83 @@
 package com.nutritime2;
 
-
 import android.os.Bundle;
+
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * Created by CarmemLucia on 21/10/2015.
- */
-    public class FragmentoA extends Fragment {
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_a, container,false);
+public class Fragmentoa extends AppCompatActivity {
+    private TabLayout mTabLayout;
+    private ViewPager mPager;
+    private MyPagerAdapter mAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragmentoa);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mAdapter= new MyPagerAdapter(getSupportFragmentManager());
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
+
+    }
+public static class MyFragment extends Fragment {
+        public MyFragment(){
+
+        }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+            TextView myText = new TextView(getActivity());
+            myText.setText("Oi eu sou o texto deste fragmento");
+            myText.setGravity(Gravity.CENTER);
+            return myText;
+
+        }
+    }
+
+
+
+}
+class MyPagerAdapter extends FragmentStatePagerAdapter{
+
+
+    public MyPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragmentoa.MyFragment myFragment= new Fragmentoa.MyFragment();
+        return myFragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 7;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position){
+
+        return super.getPageTitle(position);
     }
 }
+
+
+
+
+
